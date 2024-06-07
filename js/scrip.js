4bc1867f39bc6cf62612b85c73908a001a31485e
DGMMX <dgmmxs2@outlook.com>
2024-06-07 18:06:43 -0300

    js feito

---
+++ js/scrip.js
@@ -0,0 +1,32 @@
+let listaUsuarios = [
+    { user: "admin", senha: "123456" },
+    { user: "usersamir", senha: "123456" },
+    { user: "userandre", senha: "123456" },
+    { user: "userlevy", senha: "123456" },
+];
+
+function login(username, password) {
+    let message;
+    for (let index = 0; index < listaUsuarios.length; index++) {
+        const usuario = listaUsuarios[index];
+        if (usuario.user === username && usuario.senha === password) {
+            let mathRandom = Math.random().toString(16).substr(2);
+            let token = mathRandom + mathRandom;
+            localStorage.setItem('token', token);
+            localStorage.setItem('userLogado', JSON.stringify(usuario));
+            message = "Login efetuado com sucesso!";
+            window.location.href = "./about.html";
+            return;
+        } else {
+            message = "Credenciais inválidas!";
+        }
+    }
+    console.log(message);
+}
+
+let btnLogin = window.document.querySelector(".login_entrar");
+btnLogin.addEventListener('submit', ()=>{
+    let inputuser = window.document.querySelector("#usuaria_login");
+    let inputsenha = window.document.querySelector("#senha_login");
+    login(inputuser.value, inputsenha.value);
+});
